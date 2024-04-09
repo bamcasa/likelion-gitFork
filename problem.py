217580d8de91dfbcf095089922b07467ade9aefa
KimSehyeoun <rla6170@naver.com>
2024-04-09 20:21:38 +0900

    division by zero Error

--- problem.py
+++ problem.py
@@ -18,10 +18,12 @@ def calculator():
         print("The result is ", result)
 
     elif operation == '/':
+        if num2 == 0:
+            return;
         result = num1 / num2
         print("The result is ", result)
 
     else:
         print("Invalid operation. Please choose from +, -, *, /.")
 
-calculator()
+calculator() 

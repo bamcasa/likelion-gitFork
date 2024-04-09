e3e5871033dd422e8cd861b7a2f6f44723e0b4dd
dltnals1210 <dltnalstnals14@gmail.com>
2024-04-09 20:32:50 +0900

    debug problem.py ZeroDivisionError & InvalidInputTypeError

--- problem.py
+++ problem.py
@@ -2,26 +2,31 @@
 
 def calculator():
     operation = input("Choose the operation (+, -, *, /): ")
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
 
-    if operation == '+':
-        result = num1 + num2
-        print("The result is ", result)
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
-    elif operation == '-':
-        result = num1 - num2
-        print("The result is ", result)
+        if operation == '+':
+            result = num1 + num2
+        elif operation == '-':
+            result = num1 - num2
+        elif operation == '*':
+            result = num1 * num2
+        elif operation == '/':
+            if num2 == 0:
+                print("Error: Division by zero.")
+            result = num1 / num2
+        elif operation == '^':
+            result = num1 ** num2
 
-    elif operation == '*':
-        result = num1 * num2
-        print("The result is ", result)
+        print("The result is", result)
 
-    elif operation == '/':
-        result = num1 / num2
-        print("The result is ", result)
-
-    else:
-        print("Invalid operation. Please choose from +, -, *, /.")
+    except ValueError:
+        print("Invalid input. Please enter valid numeric values.")
+    except Exception as e:
+        print("An error occurred:", e)
 
 calculator()
+
+
